398c4b7d018ada9077e483ece701fd14b5bbc738
minhhoangtran94 <tran0450@algonquinlive.com>
2022-10-29 13:46:57 -0700

    test

--- app.config.js
+++ app.config.js
@@ -2,7 +2,7 @@ import * as dotenv from "dotenv";
 dotenv.config();
 
 module.exports = {
-  name: "Hello",
+  name: "demo-expo",
   version: process.env.MY_CUSTOM_PROJECT_VERSION || "1.0.0",
   // All values in extra will be passed to your app.
   extra: {

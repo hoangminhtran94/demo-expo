01dfc0694388ef3e08862fa4fb5ad37f1956cb54
minhhoangtran94 <tran0450@algonquinlive.com>
2022-10-29 13:44:45 -0700

    test

--- App.js
+++ App.js
@@ -1,11 +1,12 @@
 import { StatusBar } from "expo-status-bar";
 import { StyleSheet, Text, View } from "react-native";
 import React from "react";
+import Constants from "expo-constants";
 export default function App() {
   return (
     <View style={styles.container}>
       <Text>Hello!</Text>
-      <Text>{process.env.REACT_APP_API}</Text>
+      <Text>{Constants.expoConfig.extra.fact}</Text>
       <StatusBar style="auto" />
     </View>
   );
---
+++ app.config.js
@@ -0,0 +1,11 @@
+import * as dotenv from "dotenv";
+dotenv.config();
+
+module.exports = {
+  name: "Hello",
+  version: process.env.MY_CUSTOM_PROJECT_VERSION || "1.0.0",
+  // All values in extra will be passed to your app.
+  extra: {
+    fact: process.env["REACT_APP_API"],
+  },
+};

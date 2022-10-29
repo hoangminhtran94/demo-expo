cbf71302f799d6e58ac29e8f11c5ba1965daa5c6
minhhoangtran94 <tran0450@algonquinlive.com>
2022-10-29 15:28:29 -0700

    test 3

--- App.js
+++ App.js
@@ -2,10 +2,14 @@ import { StatusBar } from "expo-status-bar";
 import { StyleSheet, Text, View } from "react-native";
 import React from "react";
 import Constants from "expo-constants";
+import * as dotenv from "dotenv";
+dotenv.config();
+
 export default function App() {
   return (
     <View style={styles.container}>
       <Text>Hello!</Text>
+      <Text>{process.env["REACT_APP_API"]}</Text>
       <Text>{process.env["REACT_APP_API"]}</Text>
       {/* <Text>{Constants.expoConfig.extra.api}</Text> */}
       <StatusBar style="auto" />
